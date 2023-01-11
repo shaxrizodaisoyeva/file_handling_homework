b2852b69e8a074869388a7e0221e9aa55169e738
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-11 21:34:10 +0500

    5

--- file05.py
+++ file05.py
@@ -6,5 +6,20 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    a=data.split(",")
+    b=[]
+    c=0
+    d=0
+    while c<len(data):
+        if data[c].isdigit():
+            d+=1
+        c+=1
+    f=len(data)-d
+    b.append(d) 
+    b.append(f)
+    return b
+encoding='UTF-8'
+f=open('txt_file/data05.txt')
+data=f.read() 
+print(main(data))
 # Read data from file
