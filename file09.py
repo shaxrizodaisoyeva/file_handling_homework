89a94d992644fae7d817e8da74b65443ee00becd
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-11 21:52:49 +0500

    9

--- file09.py
+++ file09.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    a=data.split(",")
+    b=[]
+    c=0
+    while c<len(data):
+        if data[c].isdigit():
+            b+=data[c]
+            d=min(b)
+        c+=1
+    return d
+encoding='UTF-8'
+f=open('txt_file/data09.txt')
+data=f.read() 
+print(main(data))
 # Read data from file
