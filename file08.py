2f9bc8cb60187b5be8ea439ebafa3bdb6a0035ae
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-11 21:51:19 +0500

    8

--- file08.py
+++ file08.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    a=data.split(",")
+    b=[]
+    c=0
+    while c<len(data):
+        if data[c].isdigit():
+            b+=data[c]
+            d=max(b)
+        c+=1
+    return d
+encoding='UTF-8'
+f=open('txt_file/data08.txt')
+data=f.read() 
+print(main(data))
 # Read data from file
