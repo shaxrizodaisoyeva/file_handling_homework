3b4bc2a9b2b9733ce370afba332aa40f7c25ffbd
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-11 21:47:52 +0500

    7

--- file07.py
+++ file07.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    a=data.split(",")
+    c=0
+    sum=0
+    while c<len(data):
+        if data[c].isdigit():
+            sum+=int(data[c])
+        c+=1
+    return sum
+encoding='UTF-8'
+f=open('txt_file/data07.txt')
+data=f.read() 
+print(main(data))
 # Read data from file
