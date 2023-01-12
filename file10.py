f68dd3a4589d740fb4c7c52f5660ab8398885b98
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-12 09:09:31 +0500

    10

--- file10.py
+++ file10.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    b=[]
+    c=0
+    data=[data.strip() for data in data]
+    for c in data:
+        b.append(len(c))
+        d=max(b)
+    return d
+encoding='UTF-8'
+f=open('txt_file/data10.txt')
+data=f.readlines()
+print(main(data))
 # Read data from file
