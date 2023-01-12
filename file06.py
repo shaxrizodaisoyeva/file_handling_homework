a2ea2b905469be9e48396a693f4620b08b28d921
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-12 09:07:00 +0500

    6

--- file06.py
+++ file06.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    b=[]
+    c=0
+    data=[data.strip() for data in data]
+    for c in data:
+        b.append(len(c))
+    return b
+encoding='UTF-8'
+f=open('txt_file/data06.txt')
+data=f.readlines()
+print(main(data))
 # Read data from file
